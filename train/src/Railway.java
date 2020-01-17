00ac308cc1267bdbdd0a7029b47b2eae43def70a
ivojuroro <ivojuroro@gmail.com>
2020-01-17 15:57:05 +0100

    - CalculateRoute on Railway - Added setters and getters to Section

--- train/src/Railway.java
+++ train/src/Railway.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Représentation d'un circuit constitué d'éléments de voie ferrée : gare ou
  * section de voie
@@ -6,6 +10,7 @@
  * @author Philippe Tanguy <philippe.tanguy@imt-atlantique.fr>
  */
 public class Railway {
+
 	private final Element[] elements;
 
 	public Railway(Element[] elements) {
@@ -15,6 +20,10 @@ public class Railway {
 		this.elements = elements;
 		for (Element e : elements)
 			e.setRailway(this);
+	}
+
+	public Element[] getElements() {
+		return elements;
 	}
 
 	@Override
@@ -30,4 +39,19 @@ public class Railway {
 		}
 		return result.toString();
 	}
+
+	/**
+	 * Calculates the route in a linear railway. Route will include depart and destinations elements
+	 * @param originalStation
+	 * @param destination
+	 * @return
+	 */
+	public synchronized List<Element> calculateRoute(Station originalStation, Station destination){
+		List<Element> elementsList = new ArrayList<>(Arrays.asList(this.elements));
+		List<Element> finalRoute = new ArrayList<>();
+		for(int i = elementsList.indexOf(originalStation); i<= elementsList.indexOf(destination); i++)
+			finalRoute.add(elementsList.get(i));
+		return finalRoute;
+	}
+
 }
--- train/src/Section.java
+++ train/src/Section.java
@@ -6,6 +6,35 @@
  * @author Philippe Tanguy <philippe.tanguy@imt-atlantique.fr>
  */
 public class Section extends Element {
+
+	private boolean inUse;
+	private Station stationA;
+	private Station stationB;
+	private Train trainInSection;
+
+	public Section(String name, boolean inUse, Station stationA, Station stationB, Train trainInSection) {
+		super(name);
+		this.inUse = inUse;
+		this.stationA = stationA;
+		this.stationB = stationB;
+		this.trainInSection = trainInSection;
+	}
+
+	public synchronized boolean isInUse() throws InterruptedException {
+		return inUse;
+	}
+
+	public synchronized void enter() throws InterruptedException {
+		while (this.inUse){
+			wait();
+		}
+		this.inUse = true;
+	}
+
+	public synchronized boolean usedByTrain(Train train){
+		return trainInSection.equals(train);
+	}
+
 	public Section(String name) {
 		super(name);
 	}
