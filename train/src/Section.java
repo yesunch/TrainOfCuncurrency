4867f4e35a8153666cad4e0f4ace19eed0d11f86
ivojuroro <ivojuroro@gmail.com>
2020-01-19 14:22:11 +0100

    - Replacing sout for Logger

--- train/src/Section.java
+++ train/src/Section.java
@@ -1,3 +1,5 @@
+import java.util.logging.Logger;
+
 /**
  * Représentation d'une section de voie ferrée. C'est une sous-classe de la
  * classe {@link Element}.
@@ -8,33 +10,24 @@
 public class Section extends Element {
 
 	private boolean inUse;
-	private Station stationA;
-	private Station stationB;
-	private Train trainInSection;
+	private final static Logger LOGGER = Logger.getLogger(Section.class.getName());
 
-	public Section(String name, boolean inUse, Station stationA, Station stationB, Train trainInSection) {
+	public Section(String name) {
 		super(name);
-		this.inUse = inUse;
-		this.stationA = stationA;
-		this.stationB = stationB;
-		this.trainInSection = trainInSection;
+		this.inUse = false;
 	}
 
 
 	public synchronized void enter(Train train) throws InterruptedException {
 		while (this.inUse){
-			System.out.println(train.toString()+" is waiting to use section "+this.toString());
+			LOGGER.info(train.toString()+" is waiting to use section "+this.toString());
 			wait();
 		}
-		System.out.println(train.toString()+" has entered "+this.toString());
+		LOGGER.info(train.toString()+" has entered "+this.toString());
 		this.inUse = true;
 	}
 
 	public synchronized void setInUse(boolean inUse){
 		this.inUse = inUse;
-	}
-
-	public Section(String name) {
-		super(name);
 	}
 }
